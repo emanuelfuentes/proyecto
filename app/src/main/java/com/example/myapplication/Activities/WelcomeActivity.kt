e9f0884bfcb0fbc748cc27dc7447288ba9e6875e
emanuel fuentes <emanuelvasquezf@gmail.com>
2019-06-19 11:57:03 -0600

    esqueleto avanzado

---
+++ app/src/main/java/com/example/myapplication/Activities/WelcomeActivity.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.R
+
+class WelcomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
