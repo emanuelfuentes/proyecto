dddcbcd02c110ee8344a33a2b86d0c7fad3300d5
GuillermoMorales <00011417@uca.edu.sv>
2019-06-21 07:46:03 -0600

    Inflate Finished

---
+++ app/src/main/java/com/example/myapplication/Activities/Fragments/EmailFragment.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication.Activities.Fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.myapplication.R
+
+class EmailFragment: Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_register_email, container, false)
+    }
+
+
+}
