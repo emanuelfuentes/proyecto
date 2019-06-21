fbef14d268895cc9985c3900349d5c2f4ca05497
GuillermoMorales <00011417@uca.edu.sv>
2019-06-21 09:23:04 -0600

    Link Activities with Fragments

--- app/src/main/java/com/example/myapplication/Activities/WelcomeActivity.kt
+++ app/src/main/java/com/example/myapplication/Activities/WelcomeActivity.kt
@@ -1,17 +1,32 @@
 package com.example.myapplication.Activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.Navigation
+import com.example.myapplication.Activities.Fragments.NameFragment
 import com.example.myapplication.R
+import kotlinx.android.synthetic.main.activity_welcome.*
+import javax.security.auth.login.LoginException
 
 class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
-    }
 
+
+        btn_login_welcomeactivity.setOnClickListener {
+            val intent = Intent(this, LoginException::class.java)
+            // start your next activity
+            startActivity(intent)
+        }
+        btn_register_welcomeactivity.setOnClickListener {
+            val intent = Intent(this, NameFragment::class.java)
+            // start your next activity
+            startActivity(intent)
+        }
+    }
 
 
 }
