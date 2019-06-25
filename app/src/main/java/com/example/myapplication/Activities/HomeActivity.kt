d13cd390eb0fccc6d15e4deba4983788d2081322
GuillermoMorales <00011417@uca.edu.sv>
2019-06-25 15:21:01 -0600

    Menu Nav Finished

--- app/src/main/java/com/example/myapplication/Activities/HomeActivity.kt
+++ app/src/main/java/com/example/myapplication/Activities/HomeActivity.kt
@@ -2,12 +2,45 @@ package com.example.myapplication.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.example.myapplication.R
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        setUpBottomNavMenu(navController)
+        setUpSideNavigationMenu(navController)
+        setUpActionBar(navController)
     }
+    private fun setUpBottomNavMenu(navController: NavController){
+        bottom_nav?.let {
+            NavigationUI.setupWithNavController(it, navController)
+        }
+    }
+    private fun setUpSideNavigationMenu(navController: NavController){
+        nav_view?.let{
+            NavigationUI.setupWithNavController(it,navController)
+        }
+    }
+    private fun setUpActionBar(navController: NavController){
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment_home)
+        val navigated:Boolean = NavigationUI.onNavDestinationSelected(item!!, navController)
+        return navigated || super.onOptionsItemSelected(item)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(Navigation.findNavController(this, R.id.nav_host_fragment),drawer_layout)
+    }
+
 }
